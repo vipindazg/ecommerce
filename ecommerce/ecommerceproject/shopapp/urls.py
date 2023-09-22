4d0d7fdd8b7d6d3271acd420a3ac4c8b42ebf9eb
vipindazg <vipindaz1997@gmail.com>
2023-09-22 11:25:45 +0530

    addedded

---
+++ ecommerce/ecommerceproject/shopapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path , include
+from . import views
+app_name='shopapp'
+
+
+urlpatterns = [
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='product_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name='proCatdetail'),
+
+]
+
+
